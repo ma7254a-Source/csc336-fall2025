244a76bff632a28618f97df347e5da3e8130f349
ma7254a-Source <ma7254a@student.american.edu>
2025-10-06 16:49:21 -0400

    Update
    Now, user can extract today's temperature from 'Today's Weather' button. Can scale this to display chance of rain and the daily high and low.

--- 06-fetch-apis/javascript.js
+++ 06-fetch-apis/javascript.js
@@ -1,18 +1,20 @@
 //Using WeatherStack, semi-open source weather report API --> free account grants 100 calls/month
+//Asked people at work what items they look for most when they view the weather app: chance of rain, temp. highs-lows, and "current" temperature were the largest items.
 
-/*
-//basic fetch method 
+/* --basic fetch method using a simple promise chain-- 
 fetch("http://api.weatherstack.com/current?access_key=a11733a000cbeecf1937a8b10890bd36&query=New%20York")
 .then(res=>res.json()) //"response (<--our response object) --> convert to json"
 .then(data => console.log(data)) //"data --> print data"
 .catch(err => console.error(err)); //"If theres an error, print it."
-//^^ Simple Promise Chain 
+--Call async-wait function--
+fetchData();
 */
 
-//fetch using async-wait function
+
+//fetch function using async-await method 
 async function fetchData(){
-    //try and catch block
     /*the fetch function will return an object, 'response' object. We will await a promise, returned by fetch() */
+    //try and catch block
     try {
         const response = await fetch("http://api.weatherstack.com/current?access_key=a11733a000cbeecf1937a8b10890bd36&query=New%20York"); 
         //Once this resolves, we will have to see if the response is okay
@@ -20,19 +22,30 @@ async function fetchData(){
             throw new Error("Could not fetch resource.");
         }
         //if it is okay, we will create a const, data, and convert it to json
-        const data = await response.json();
-        const tempC = data.current.temperature;
+        const data = await response.json(); //this line here converts the raw data to JSON
         console.log(data);
+        const tempC = data.current.temperature; //Now I can extract whatever single pieces of data I want
         console.log(tempC);
-    }
+        return tempC; //trying to access tempC while considering variable scope... 
+   }
     /*one parameter, an error, if we recieve an error--lets console that error via error*/
     catch(error){
         console.error(error);
     }
 }
 
+document.getElementById('button').addEventListener("click", async () => {
+    try {
+        const tempC = await fetchData();
+        document.getElementById("temp").textContent = `${tempC}`;
+    } 
+    catch (error) {
+        console.error(error);
+    }
+}); 
 
 
+//i'll come back to this. 
 function extractData() {
     //function to display data fetched from extractData()  
 
