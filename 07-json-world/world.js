1493de62fad70bcdbb7b57a9fa049fe9be0f0bba
ma7254a-Source <ma7254a@student.american.edu>
2025-10-14 11:34:05 -0400

    Update
    Going to write script to find rarity of items and print it.

--- 07-json-world/world.js
+++ 07-json-world/world.js
@@ -2,5 +2,8 @@ import fs from 'fs';
 
 const data = fs.readFileSync('world.json', 'utf-8');
 
-const towns = JSON.parse(data);
-console.log(towns);
+const towns = JSON.parse(data); 
+/*
+So we've imported the fs module using es module importation, read the json file, and parsed into a readable object.
+Now all I have to do it write a script to read through and print something from the file. I'm thinking I'll just print every item object with the 'rare' rarity value.
+*/
