9908669fb13488d585c1fd4e1eaa80b69baa8f78
ma7254a-Source <ma7254a@student.american.edu>
2025-10-14 12:07:35 -0400

    Finished
    Completed for loop, prints rare items, their owners, and where they are from.

--- 07-json-world/world.js
+++ 07-json-world/world.js
@@ -2,8 +2,21 @@ import fs from 'fs';
 
 const data = fs.readFileSync('world.json', 'utf-8');
 
-const towns = JSON.parse(data); 
+const worlds = JSON.parse(data); 
 /*
 So we've imported the fs module using es module importation, read the json file, and parsed into a readable object.
 Now all I have to do it write a script to read through and print something from the file. I'm thinking I'll just print every item object with the 'rare' rarity value.
 */
+
+//for loop iterates over each 'layer' in our parsed json world, used class example to map out the loop.
+for (const region of worlds.regions) {
+    for (const town of region.towns) {
+        for (const person of town.notable_people) {
+            for (const item of person.items) {
+                if (item && typeof item == "object" && item.rarity == "rare") { //I included both objects and strings in my json file so I need to ignore string items since they have no rarity
+                    console.log(`Rare Items: ${item.name} (rarity: ${item.rarity}) - Item Owner: ${person.name}, Town: ${town.name}, Region: ${region.name}`);
+                }
+            }
+        }
+    }
+}
